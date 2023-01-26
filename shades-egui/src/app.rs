296902d3c284d5e4c6e477db00e4e7d7e2de4832
Igor Loskutov <igor.loskutoff@gmail.com>
2023-01-26 16:09:05 +0700

    add egui build

--- shades-egui/src/app.rs
+++ shades-egui/src/app.rs
@@ -103,14 +103,5 @@ impl eframe::App for TemplateApp {
             ));
             egui::warn_if_debug_build(ui);
         });
-
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally choose either panels OR windows.");
-            });
-        }
     }
 }
