4bfa83c1919bc641fbb25c4005dbc31690c295b7
Igor Loskutov <igor.loskutoff@gmail.com>
2023-02-10 22:10:10 +0700

    add leptos

---
+++ shades-leptos/src/main.rs
@@ -0,0 +1,9 @@
+use leptos::*;
+use shades_leptos::*;
+
+pub fn main() {
+    console_error_panic_hook::set_once();
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    mount_to_body(|cx| view! { cx, <RouterExample/> })
+}
